02154f12c3fc4bd07ac005ead1e9efdaf439cfd3
Stanislawische <52015061+Stanislawische@users.noreply.github.com>
2025-01-18 14:41:26 +0300

    vercel_analytics

--- shared/components/providers.tsx
+++ shared/components/providers.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 import NextTopLoader from 'nextjs-toploader';
+import { Analytics } from '@vercel/analytics/next';
 
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
 	return (
@@ -12,6 +13,7 @@ export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
 			<Toaster />
 			<NextTopLoader 
 			height={5}/>
+			<Analytics />
 		</>
 	);
 };
