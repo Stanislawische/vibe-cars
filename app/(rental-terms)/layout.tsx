2416beda1b99033710e62fa314c324bdb4456835
Stanislawische <52015061+Stanislawische@users.noreply.github.com>
2025-01-19 15:24:50 +0300

    rental fix

---
+++ app/(rental-terms)/layout.tsx
@@ -0,0 +1,33 @@
+import { Suspense } from 'react';
+import '../globals.css';
+import { Container, Header } from '@/shared/components/shared';
+
+export const metadata = {
+	title: 'Vibe Cars | Корзина',
+};
+
+/**
+ * Макет для страниц оформления заказа.
+ *
+ * @example
+ * <CheacoutLayout>
+ *   <MyCheckoutPage />
+ * </CheacoutLayout>
+ */
+export default function RentaltLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<main className="min-h-screen min-w-[1090px] antialiased bg-zinc-950">
+			
+				<Suspense>
+					<Header
+						hasSearch={false}
+					/>
+				</Suspense>
+				{children}
+		</main>
+	);
+}
